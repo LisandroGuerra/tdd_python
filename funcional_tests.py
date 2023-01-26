41a8ff8051695e7bcdc551c76453fbcf4a9de76d
Lisandro Guerra <lisandro.digital@gmail.com>
2023-01-26 21:05:47 +0000

    FT commentaries

--- funcional_tests.py
+++ funcional_tests.py
@@ -2,8 +2,32 @@ from selenium import webdriver
 
 
 browser = webdriver.Firefox()
+
+# Lisandro was looking for a to-do app and decides to check-out the homepage
 browser.get('http://localhost:8000')
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+# He realizes that page the title an the header mention to-do list
+assert 'To-Do' in browser.title
 
-browser.close()
+# He is invited to immediately insert a task item
+
+# He types "Buy a new pillow" in a text box
+
+# When he types "enter" the page is updated and now it lists
+# "1: Buy a new pillow" as an item in the to-do list
+
+# Still have a text box inviting him to add another item
+# He types "Buy a pillow cover to my pillow"
+
+# The page is updated and now shows two tasks in the to-do list
+
+# Lisandro asks to him self if the site will remember his list. 
+# Than he notices that the site generates a unique URL to him.
+# There is a short text explaning this
+
+# He accesses the URL and his to-do list still there
+
+# Satisfied he goes back to sleep
+
+browser.quit()
+# browser.close()
