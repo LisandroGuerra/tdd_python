ed58002bca266fa1c4391d33700037dedcadcf1d
Lisandro Guerra <lisandro.digital@gmail.com>
2023-01-26 23:04:56 +0000

    unittest added

--- funcional_tests.py
+++ funcional_tests.py
@@ -1,33 +1,47 @@
+import unittest
 from selenium import webdriver
 
+class NewVisitorTest(unittest.TestCase):
 
-browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# Lisandro was looking for a to-do app and decides to check-out the homepage
-browser.get('http://localhost:8000')
 
-# He realizes that page the title an the header mention to-do list
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-# He is invited to immediately insert a task item
 
-# He types "Buy a new pillow" in a text box
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Lisandro was looking for a to-do app and decides to check-out the homepage
+        self.browser.get('http://localhost:8000')
 
-# When he types "enter" the page is updated and now it lists
-# "1: Buy a new pillow" as an item in the to-do list
+        # He realizes that page the title an the header mention to-do list
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test.')
+        
 
-# Still have a text box inviting him to add another item
-# He types "Buy a pillow cover to my pillow"
+        # He is invited to immediately insert a task item
 
-# The page is updated and now shows two tasks in the to-do list
+        # He types "Buy a new pillow" in a text box
 
-# Lisandro asks to him self if the site will remember his list. 
-# Than he notices that the site generates a unique URL to him.
-# There is a short text explaning this
+        # When he types "enter" the page is updated and now it lists
+        # "1: Buy a new pillow" as an item in the to-do list
 
-# He accesses the URL and his to-do list still there
+        # Still have a text box inviting him to add another item
+        # He types "Buy a pillow cover to my pillow"
 
-# Satisfied he goes back to sleep
+        # The page is updated and now shows two tasks in the to-do list
 
-browser.quit()
-# browser.close()
+        # Lisandro asks to him self if the site will remember his list. 
+        # Than he notices that the site generates a unique URL to him.
+        # There is a short text explaning this
+
+        # He accesses the URL and his to-do list still there
+
+        # Satisfied he goes back to sleep
+
+
+        # browser.close()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
